4b75c61ba6a0f893f9ef180a166079c6a4b81fda
Reid  Loeffler <reidloeffler@email.arizona.edu>
2021-10-05 21:11:25 -0700

    program that determines if an item, entered by the user, allready exist in the look up table

--- senior_design.py
+++ senior_design.py
@@ -10,10 +10,10 @@ def calculate_total_lines(file):
     total_lines = 0
 
     for char_num in range(len(file) - 1):
-        if file[char_num] == '\t':
+        if file[char_num] == '\n':
             total_lines += 1
 
-    total_lines = int(total_lines / 3)
+    total_lines = int(total_lines / 2) + 1
 
     return total_lines
 
@@ -23,26 +23,22 @@ def calculate_total_lines(file):
 
 def get_values(file, total_lines, usr_description):
 
-    info = dict(line_num=0, description='', volume='', mass=0)
+    info = dict(description='', concentration='')
     total_num_line = 0
     num_line = 0
     char_num = 0
     description = ''
-    volume = ''
-    mass = ''
+    concentration = ''
 
-    while total_num_line < total_lines:
+    while total_num_line <= total_lines:
 
-        if file[char_num] != '\n' and file[char_num] != '\t':
+        if file[char_num] != '\n':
 
             if num_line == 0:
                 description += file[char_num]
 
             if num_line == 1:
-                volume += file[char_num]
-
-            if num_line == 2:
-                mass += file[char_num]
+                concentration += file[char_num]
 
             char_num += 1
 
@@ -50,29 +46,23 @@ def get_values(file, total_lines, usr_description):
             num_line += 1
             char_num += 1
 
-        if file[char_num] == '\t':
-            char_num += 1
-
-        if num_line == 3:
+        if num_line == 2:
             total_num_line += 1
             num_line = 0
-            info['description'] = description
-            info['volume'] = volume
-            info['mass'] = mass
 
-            if usr_description == (info['description'] + '\n'):
-                info['line_num'] = total_num_line
-                info['volume'] = volume
-                info['mass'] = mass
+            if usr_description == (description + '\n'):
+                info['description'] = description
+                info['concentration'] = concentration
                 total_num_line = total_lines
 
-            description = ''
-            volume = ''
-            mass = ''
+                return [info['description'], info['concentration']]
 
-    return [
-        info['line_num'], info['description'], info['volume'], info['mass']
-    ]
+            else:
+                description = ''
+                concentration= ''
+
+                if total_num_line == total_lines:
+                    return ('entry does not exist')
 
 
 # --------------------------------------------------
@@ -80,18 +70,19 @@ def get_values(file, total_lines, usr_description):
 
 def main():
 
-    look_up_table = open('Look_Up_Table.txt').read()
-
+    look_up_table = open('look_up_table.txt').read()
     total_lines = calculate_total_lines(look_up_table)
 
+    print('Please enter and item description to search for:')
     usr_description = sys.stdin.readline()
-
     info = get_values(look_up_table, total_lines, usr_description)
 
-    print('Line Number: ' + str(info[0]))
-    print('Descroption: ' + info[1])
-    print('Volume: ' + info[2])
-    print('Mass: ' + info[3])
+    if info == 'entry does not exist':
+        print('Entry does not exist. Would you like to enter a new item?')
+
+    else:
+        print('Description: ' + info[0])
+        print('Concentration: ' + info[1])
 
 
 # --------------------------------------------------
